3648629fcfc90d52eeb1b4abd7ef9b4e23b8182b
AmandaLu <amanda.cx.lu@gmail.com>
2024-08-22 14:47:17 -0700

    prob 07 solved

--- problems/07-max-reduce.js
+++ problems/07-max-reduce.js
@@ -16,6 +16,8 @@ console.log(maxWithReduce([-2, -3, -7, 3])); // 3
 
 let maxWithReduce = function(nums) {
     // Your code here
+    let max = nums.reduce((a, b) => Math.max(a, b), -Infinity);
+    return max;
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
