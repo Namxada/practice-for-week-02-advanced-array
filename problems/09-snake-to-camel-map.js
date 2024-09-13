5977f691d9f52fd8ffccda56420af8019734ae67
AmandaLu <amanda.cx.lu@gmail.com>
2024-09-12 22:34:03 -0700

    solved 09

--- problems/09-snake-to-camel-map.js
+++ problems/09-snake-to-camel-map.js
@@ -17,6 +17,11 @@ console.log(snakeToCamel('APp_ACADEMY_iS_cOol')); // 'AppAcademyIsCool'
 
 function snakeToCamel(str) {
     // Your code here
+    let splitWords = str.split('_');
+    let pascalCased = splitWords.map(function(word){
+        return word[0].toUpperCase() + word.slice(1).toLowerCase();
+    });
+    return pascalCased.join('');
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
@@ -25,4 +30,4 @@ try {
     module.exports = snakeToCamel;
 } catch (e) {
     module.exports = null;
-}
+}
